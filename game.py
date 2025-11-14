a4a0f49d1c3862190ada6915c1aa5c0e8d672b89
subina thapa <tsubina57@gmail.com>
2025-11-14 18:08:32 +0545

    Update game.py

--- game.py
+++ game.py
@@ -8,6 +8,6 @@ user_choice = int(input("Guess a number between 1 and 10: "))
 
 
 if user_choice == random_number:
-    print(" Congratulations! You guessed it right.")
+    print(" Congratulations You guessed it right.")
 else:
     print(f" Oops! Wrong guess. The correct number was {random_number}.")
